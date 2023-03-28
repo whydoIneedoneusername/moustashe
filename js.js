28b0451bfe03b0807af8bdbe1c99569aafc92482
whydoIneedoneusername <100236031+whydoIneedoneusername@users.noreply.github.com>
2023-03-28 17:58:00 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas=canvasCreate(280,280)
+    canvas.center();
+}
+
+
+function draw()
+{
+    
+}
+
+function onclick()
+{
+    take_snapshot()
+}
